65b7ac11a767b6b7cf6b116feefdeea519f53f37
Nur Hidayatullah <kematjaya0@gmail.com>
2021-06-07 07:51:08 +0700

    update to 5.3

--- src/Entity/KmjUser.php
+++ src/Entity/KmjUser.php
@@ -18,7 +18,7 @@ abstract class KmjUser implements KmjUserInterface
      * @ORM\Id
      * @ORM\Column(type="uuid", unique=true)
      * @ORM\GeneratedValue(strategy="CUSTOM")
-     * @ORM\CustomIdGenerator(class=Symfony\Bridge\Doctrine\IdGenerator\UuidV4Generator::class)
+     * @ORM\CustomIdGenerator(class=Symfony\Bridge\Doctrine\IdGenerator\UuidGenerator::class)
      */
     private $id;
 
