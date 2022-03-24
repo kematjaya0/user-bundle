9cd37b6c132f5b35fef56a7074ff2dd95aa7b304
Nur Hidayatullah <kematjaya0@gmail.com>
2022-03-24 14:26:14 +0700

    update config file

---
+++ src/EventListener/UnauthorizedEventListener.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+namespace Kematjaya\UserBundle\EventListener;
+
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\Security\Core\Exception\InsufficientAuthenticationException;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+/**
+ * Description of UnauthorizedEventListener
+ *
+ * @author guest
+ */
+class UnauthorizedEventListener 
+{
+    /**
+     * 
+     * @var UrlGeneratorInterface
+     */
+    private $urlGenerator;
+    
+    public function __construct(UrlGeneratorInterface $urlGenerator) 
+    {
+        $this->urlGenerator = $urlGenerator;
+    }
+    
+    public function onKernelException(ExceptionEvent $event)
+    {
+        if ($event->getThrowable() instanceof InsufficientAuthenticationException) {
+            $this->createResponse($event);
+        }
+        
+        if ($event->getThrowable()->getPrevious() instanceof InsufficientAuthenticationException) {
+            $this->createResponse($event);
+        }
+    }
+    
+    protected function createResponse(ExceptionEvent $event)
+    {
+        $event->getRequest()->getSession()->getFlashBag()->add("error", $event->getThrowable()->getMessage());
+        $response = new RedirectResponse(
+            $this->urlGenerator->generate("kmj_user_login")
+        );
+        
+        $event->setResponse($response);
+    }
+}
